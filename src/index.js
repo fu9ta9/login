0fb60768b0cb5b7b1de4972fefa0c5c77a16f93d
fu9ta9 <66508866+fu9ta9@users.noreply.github.com>
2023-09-10 13:30:11 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Amplify from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 import config from './aws-exports';
-Amplify.configure(config);- src/App.jsに認証フローを追加
-
+Amplify.configure(config);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
